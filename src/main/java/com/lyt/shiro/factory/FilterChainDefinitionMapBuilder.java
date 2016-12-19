9bcc557c06d30c1535cd6d723855c97bfde0d454
liyintao520 <lyt890916wlj@sina.com>
2016-12-19 18:04:28 +0800

    version 1.0.2

--- src/main/java/com/lyt/shiro/factory/FilterChainDefinitionMapBuilder.java
+++ src/main/java/com/lyt/shiro/factory/FilterChainDefinitionMapBuilder.java
@@ -10,9 +10,10 @@ public class FilterChainDefinitionMapBuilder {
 		map.put("/login.jsp", "anon");
 		map.put("/shiro/login", "anon");
 		map.put("/shiro/logout", "logout");
-		map.put("/user.jsp", "authc,roles[user]");
-		map.put("/admin.jsp", "authc,roles[admin]");
-		map.put("/list.jsp", "user");
+		//在 ShiroHandler中  token.setRememberMe(true);//这个是记住用户，当用户登出之后才会取消
+		map.put("/user.jsp", "authc,roles[user]");//认证之后，还有user权限
+		map.put("/admin.jsp", "authc,roles[admin]");//认证之后，还要有amdin权限
+		map.put("/list.jsp", "user");//这个是只要通过（记住我登陆的--subject.isRemembered()）就可以方法
 		
 		map.put("/**", "authc");
 		
--- src/main/java/com/lyt/shiro/realms/ShiroRealm.java
+++ src/main/java/com/lyt/shiro/realms/ShiroRealm.java
@@ -1,5 +1,8 @@
 package com.lyt.shiro.realms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -7,38 +10,53 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
-public class ShiroRealm extends AuthenticatingRealm{
+public class ShiroRealm extends AuthorizingRealm {
 
 	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+	protected AuthenticationInfo doGetAuthenticationInfo(
+			AuthenticationToken token) throws AuthenticationException {
 		System.out.println("[FirstRealm] doGetAuthenticationInfo");
-		System.out.println("登录之后调用这个方法--doGetAuthenticationInfo:" + token.hashCode() + ", 字符串：" + token.toString());
+		
 		//1. 把 AuthenticationToken 转换为 UsernamePasswordToken 
 		UsernamePasswordToken upToken = (UsernamePasswordToken) token;
-		//2.从 UsernamePasswordToken 中获取username
+		
+		//2. 从 UsernamePasswordToken 中来获取 username
 		String username = upToken.getUsername();
-		//3.与数据库对比。调用数据库的方法，从数据库中查询username 对应的用户记录
+		
+		//3. 调用数据库的方法, 从数据库中查询 username 对应的用户记录
 		System.out.println("从数据库中获取 username: " + username + " 所对应的用户信息.");
+		
 		//4. 若用户不存在, 则可以抛出 UnknownAccountException 异常
 		if("unknown".equals(username)){
 			throw new UnknownAccountException("用户不存在!");
 		}
+		
 		//5. 根据用户信息的情况, 决定是否需要抛出其他的 AuthenticationException 异常. 
 		if("monster".equals(username)){
 			throw new LockedAccountException("用户被锁定");
 		}
+		
 		//6. 根据用户的情况, 来构建 AuthenticationInfo 对象并返回. 通常使用的实现类为: SimpleAuthenticationInfo
 		//以下信息是从数据库中获取的.
 		//1). principal: 认证的实体信息. 可以是 username, 也可以是数据表对应的用户的实体类对象. 
 		Object principal = username;
 		//2). credentials: 密码. 
-		Object credentials = "fc1709d0a95a6be30bc5926fdb7f22f4"; //"fc1709d0a95a6be30bc5926fdb7f22f4";
+		Object credentials = null; //"fc1709d0a95a6be30bc5926fdb7f22f4";
+		if("admin".equals(username)){
+			credentials = "038bdaf98f2037b31f1e75b5b4c9b26e";
+		}else if("user".equals(username)){
+			credentials = "098d2c478e9c11555ce2823231e02ec1";
+		}
+		
 		//3). realmName: 当前 realm 对象的 name. 调用父类的 getName() 方法即可
-		String realmName = ShiroRealm.class.getName();
+		String realmName = getName();
 		//4). 盐值. 
 		ByteSource credentialsSalt = ByteSource.Util.bytes(username);
 		
@@ -46,15 +64,37 @@ public class ShiroRealm extends AuthenticatingRealm{
 		info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
 		return info;
 	}
-	
+
 	public static void main(String[] args) {
-		//在spring的配置文件中有定义：id="jdbcRealm"
-		String hashedCredentials = "MD5";//加密算法
-		Object credentials = "123456";//读取前台的明文密码
-		//盐值加密（比如不同用户，数据库加密后的密码相同，这就需要盐值加密了）
-		Object salt = ByteSource.Util.bytes("user");// admin和user
-		int hashInterations = 1024;//加密次数
-		Object result = new SimpleHash(hashedCredentials, credentials, salt, hashInterations);
-		System.out.println("加密1024次后的密码：" + result);
+		String hashAlgorithmName = "MD5";
+		Object credentials = "123456";
+		Object salt = ByteSource.Util.bytes("user");;
+		int hashIterations = 1024;
+		
+		Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
+		System.out.println(result);
+	}
+
+	//授权会被 shiro 回调的方法
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(
+			PrincipalCollection principals) {
+		//1. 从 PrincipalCollection 中来获取登录用户的信息
+		Object principal = principals.getPrimaryPrincipal();
+		
+		//2. 利用登录的用户的信息来用户当前用户的角色或权限(可能需要查询数据库)
+		Set<String> roles = new HashSet<>();
+		//如果登录用户是admin，那么admin拥有user和admin两个角色的权限，如果是user，那么只有user的权限，
+		//比如admin登陆，可以访问admin.jsp和user.jsp页面。而user角色只能访问user.jsp页面
+		roles.add("user");
+		if("admin".equals(principal)){
+			roles.add("admin");
+		}
+		
+		//3. 创建 SimpleAuthorizationInfo, 并设置其 reles 属性.
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roles);
+		
+		//4. 返回 SimpleAuthorizationInfo 对象. 
+		return info;
 	}
 }
